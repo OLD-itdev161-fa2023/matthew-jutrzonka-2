899a5c272fdc6503529b073e7d02837f9d28c65c
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2022-11-15 14:50:39 -0600

    PostList and Post Components

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
 
 class App extends React.Component {
   state = {
     posts: [],
+    post: null,
     token: null,
     user: null
   };
@@ -49,7 +52,7 @@ class App extends React.Component {
           console.error(`Error logging in: ${error}`);
         })
     }
-  }
+  };
 
   loadData = () => {
     const { token } = this.state;
@@ -77,10 +80,17 @@ class App extends React.Component {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     this.setState({ user: null, token: null });
-  }
+  };
+
+  viewPost = post => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  };
 
   render() {
-    let { user, posts, post, token } = this.state;
+    let { user, posts, post } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     };
@@ -107,28 +117,30 @@ class App extends React.Component {
             </ul>
           </header>
           <main>
+            <Switch>
               <Route exact path="/">
                 {user ? (
                   <React.Fragment>
                     <div>Hello {user}!</div>
-                    <PostList
-                      posts={posts}
-                      clickPost={this.viewPost}
-                      deletePost={this.deletePost}
-                      editPost={this.editPost}
-                    />
+                    <PostList posts={posts} clickPost={this.viewPost} />
                   </React.Fragment>
                 ) : (
                   <React.Fragment>Please Register or Login</React.Fragment>
                 )}
               </Route>
-            <Switch>
-              <Route 
-                exact path="/register" 
-                render={() => <Register {...authProps} />} />
-              <Route 
-                exact path="/login" 
-                render={() => <Login {...authProps} />} />
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+              <Route
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={() => <Login {...authProps} />}
+              />
             </Switch>
           </main>
         </div>
